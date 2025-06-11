89b8e03cf5b584a9e108d139864e97a173b11469
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-11 11:05:09 -0400

    did code to Console.java and User_Interface.java classes

--- src/main/java/com/pluralsight/UI/User_Interface.java
+++ src/main/java/com/pluralsight/UI/User_Interface.java
@@ -4,12 +4,28 @@ import com.pluralsight.Models.Employee;
 
 public class User_Interface {
 
-    public void display() {
-        System.out.println("Welcome to the Northwind Database");
+    Employee currentEmployee;
+    private Console console;
 
+    public User_Interface() {
+        this.console = new Console();
     }
 
-    private Employee loginUser(){
+    public void display() {
+        System.out.println("Welcome to the Northwind Database");
+        currentEmployee = loginUser();
+        System.out.println("Welcome, " + this.currentEmployee.getFirstName() + "!");
 
+        showHomeMenu();
+    }
+
+    private Employee loginUser() {
+        String s = console.promptForString("Just hit enter to login", true);
+        Employee e = new Employee(1, "Matt", "Chris");
+        return e;
+    }
+
+    private void showHomeMenu() {
+        System.out.println("Home Menu Here");
     }
 }
