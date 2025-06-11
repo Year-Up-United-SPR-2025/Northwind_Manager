f2569e760bbbd75c83080c0eecd2d11897748413
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-11 09:30:29 -0400

    add models, ui, and build out Employee

---
+++ src/main/java/com/pluralsight/UI/User_Interface.java
@@ -0,0 +1,15 @@
+package com.pluralsight.UI;
+
+import com.pluralsight.Models.Employee;
+
+public class User_Interface {
+
+    public void display() {
+        System.out.println("Welcome to the Northwind Database");
+
+    }
+
+    private Employee loginUser(){
+
+    }
+}
